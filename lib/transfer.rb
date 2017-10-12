d1b05da66b3b3fc47c76684cd74c5c2dc83190ba
Abe Rudy <aberudy@gmail.com>
2017-10-12 14:58:20 -0400

    finished adding class methods, finished lab

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,17 +12,33 @@ class Transfer
   end
 
   def valid?
-    sender.valid? && receiver.valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+
+  def overdrawn?
+    balance = self.sender.balance
+    new_balance = balance - self.amount
+    new_balance < 0
   end
 
   def execute_transaction
-      if self.status == "pending" && self.sender.valid?
+      if self.status == "pending" && !self.overdrawn?
         self.sender.balance -= self.amount
         self.receiver.balance += self.amount
         self.status = "complete"
-      elsif
+      else
+        self.status = "rejected"
         "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+      if self.status == "complete"
+        self.sender.balance += self.amount
+        self.receiver.balance -= self.amount
+        self.status = "reversed"
+      end
+    end
+
 
 end
