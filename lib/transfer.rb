68e1ba7041747fdc06731e3867d6b8dcb442974d
Abe Rudy <aberudy@gmail.com>
2017-10-12 14:43:09 -0400

    bank account class methods complete

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :pending, :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+      if self.status == "pending" && self.sender.valid?
+        self.sender.balance -= self.amount
+        self.receiver.balance += self.amount
+        self.status = "complete"
+      elsif
+        "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
